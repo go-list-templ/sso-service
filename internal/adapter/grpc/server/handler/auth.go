c911951ded015f248e8fe16b7a4e54578ec41266
Denis <korbakovd@gmail.com>
2026-03-26 19:16:51 +0300

    feat: add logic in register handler

--- internal/adapter/grpc/server/handler/auth.go
+++ internal/adapter/grpc/server/handler/auth.go
@@ -6,6 +6,7 @@ import (
 	v1 "github.com/go-list-templ/proto/gen/api/sso/v1"
 	pbgrpc "google.golang.org/grpc"
 
+	"github.com/go-list-templ/sso-service/internal/core/dto"
 	"github.com/go-list-templ/sso-service/internal/port"
 	"go.uber.org/zap"
 )
@@ -13,19 +14,32 @@ import (
 type Auth struct {
 	v1.AuthServiceServer
 
-	authService port.AuthService
-	logger      *zap.Logger
+	service port.AuthService
+	logger  *zap.Logger
 }
 
 func RegisterAuth(s *pbgrpc.Server, a port.AuthService, l *zap.Logger) {
-	service := &Auth{authService: a, logger: l}
+	service := &Auth{service: a, logger: l}
 	{
 		v1.RegisterAuthServiceServer(s, service)
 	}
 }
 
 func (a *Auth) Register(ctx context.Context, request *v1.RegisterRequest) (*v1.RegisterResponse, error) {
-	return nil, nil
+	input := dto.AuthInput{
+		Email:    request.GetEmail(),
+		Password: request.GetPassword(),
+	}
+
+	output, err := a.service.Register(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	return &v1.RegisterResponse{
+		AccessToken:  output.AccessToken,
+		RefreshToken: output.RefreshToken,
+	}, nil
 }
 
 func (a *Auth) Login(ctx context.Context, request *v1.LoginRequest) (*v1.LoginResponse, error) {

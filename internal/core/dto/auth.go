94fd6804ce2d1ed7e9d714f1c4b54a18ab5162df
Denis <korbakovd@gmail.com>
2026-03-26 19:17:08 +0300

    feat: add validate for dto

--- internal/core/dto/auth.go
+++ internal/core/dto/auth.go
@@ -1,12 +1,33 @@
 package dto
 
+import (
+	"fmt"
+	"net/mail"
+)
+
+const MinLenPass = 8
+
 type (
 	AuthInput struct {
-		email    string
-		password string
+		Email    string
+		Password string
 	}
 
 	AuthOutput struct {
-		token string
+		AccessToken  string
+		RefreshToken string
 	}
 )
+
+func (a *AuthInput) Validate() error {
+	_, err := mail.ParseAddress(a.Email)
+	if err != nil {
+		return err
+	}
+
+	if len(a.Password) < MinLenPass {
+		return fmt.Errorf("min lenght is %v chars", MinLenPass)
+	}
+
+	return nil
+}

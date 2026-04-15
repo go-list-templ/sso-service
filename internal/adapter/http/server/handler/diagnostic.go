a0fd227c4560e1be0f42b0fa4c65ecd6259afecc
Denis <korbakovd@gmail.com>
2026-04-15 21:12:06 +0300

    fix: delete extra data

--- internal/adapter/http/server/handler/diagnostic.go
+++ internal/adapter/http/server/handler/diagnostic.go
@@ -1,68 +1,17 @@
 package handler
 
-import (
-	"context"
-	"encoding/json"
-	"net/http"
-	"time"
+import "net/http"
 
-	"github.com/go-list-templ/sso-service/internal/adapter/persistence/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
-	"go.uber.org/zap"
-)
+type Diagnostic struct{}
 
-const (
-	DefaultCtxTime   = 5 * time.Second
-	MessageServerErr = "server error"
-)
+func RegisterDiagnostic() {
+	d := &Diagnostic{}
 
-type Diagnostic struct {
-	logger *zap.Logger
-	mongo  *mongo.Mongo
+	http.HandleFunc("/healthz", d.Health())
 }
 
-func RegisterDiagnostic(l *zap.Logger, m *mongo.Mongo) {
-	d := &Diagnostic{l, m}
-
-	http.HandleFunc("/healthz", d.HealthZ())
-}
-
-func (d *Diagnostic) HealthZ() func(http.ResponseWriter, *http.Request) {
+func (d *Diagnostic) Health() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, _ *http.Request) {
-		status := http.StatusOK
-
-		ctx, cancel := context.WithTimeout(context.Background(), DefaultCtxTime)
-		defer cancel()
-
-		err := d.mongo.Ping(ctx, readpref.Nearest())
-		if err != nil {
-			status = http.StatusServiceUnavailable
-
-			d.logger.Error("pinging mongo", zap.Error(err))
-		}
-
-		data := map[string]int{
-			"status": status,
-		}
-
-		d.writeJSON(w, status, data)
-	}
-}
-
-func (d *Diagnostic) writeJSON(w http.ResponseWriter, code int, v any) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-
-	data, err := json.Marshal(v)
-	if err != nil {
-		d.logger.Error("json marshal", zap.Error(err))
-		http.Error(w, MessageServerErr, http.StatusInternalServerError)
-		return
-	}
-
-	_, err = w.Write(data)
-	if err != nil {
-		d.logger.Error("write json", zap.Error(err))
-		http.Error(w, MessageServerErr, http.StatusInternalServerError)
+		w.WriteHeader(http.StatusOK)
 	}
 }

28fe8c6ef083a31d51da108d4a29d28092b47a67
Denis <korbakovd@gmail.com>
2026-04-17 01:02:59 +0300

    fix: delete validate

--- internal/core/dto/auth.go
+++ internal/core/dto/auth.go
@@ -1,12 +1,5 @@
 package dto
 
-import (
-	"fmt"
-	"net/mail"
-)
-
-const MinLenPass = 8
-
 type (
 	AuthInput struct {
 		Email    string
@@ -18,16 +11,3 @@ type (
 		RefreshToken string
 	}
 )
-
-func (a *AuthInput) Validate() error {
-	_, err := mail.ParseAddress(a.Email)
-	if err != nil {
-		return err
-	}
-
-	if len(a.Password) < MinLenPass {
-		return fmt.Errorf("min lenght is %v chars", MinLenPass)
-	}
-
-	return nil
-}

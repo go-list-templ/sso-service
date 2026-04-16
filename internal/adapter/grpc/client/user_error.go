24f25cde35a3c1c3af0c8be914286cc02ba2c1eb
Denis <korbakovd@gmail.com>
2026-04-17 01:03:47 +0300

    feat: add new field

--- internal/adapter/grpc/client/user_error.go
+++ internal/adapter/grpc/client/user_error.go
@@ -3,20 +3,21 @@ package client
 import "errors"
 
 var (
-	ErrUserExists          = errors.New("exists")
-	ErrUserInvalidArgument = errors.New("invalid argument")
+	ErrUserExists          = errors.New("user already exists")
+	ErrUserInvalidArgument = errors.New("user invalid argument")
 )
 
 type UserExists struct {
-	Err error
+	Message string
+	Err     error
 }
 
-func NewUserExists(err error) *UserExists {
-	return &UserExists{err}
+func NewUserExists(m string, err error) *UserExists {
+	return &UserExists{m, err}
 }
 
 func (u *UserExists) Error() string {
-	return u.Err.Error()
+	return u.Message
 }
 
 func (u *UserExists) Unwrap() error {
@@ -24,15 +25,16 @@ func (u *UserExists) Unwrap() error {
 }
 
 type UserInvalidArgument struct {
-	Err error
+	Message string
+	Err     error
 }
 
-func NewUserInvalidArgument(err error) *UserInvalidArgument {
-	return &UserInvalidArgument{err}
+func NewUserInvalidArgument(m string, err error) *UserInvalidArgument {
+	return &UserInvalidArgument{m, err}
 }
 
 func (u *UserInvalidArgument) Error() string {
-	return u.Err.Error()
+	return u.Message
 }
 
 func (u *UserInvalidArgument) Unwrap() error {

23a46f5ce412fb5a7f64e0f635750cdf2a6dbbbe
Denis <korbakovd@gmail.com>
2026-04-19 00:51:08 +0300

    feat: create dao

---
+++ internal/adapter/persistence/mongo/repo/dao/session.go
@@ -0,0 +1,26 @@
+package dao
+
+import (
+	"time"
+
+	"github.com/go-list-templ/sso-service/internal/core/domain/entity"
+	"github.com/google/uuid"
+)
+
+type Session struct {
+	ID           uuid.UUID `json:"id"`
+	UserID       uuid.UUID `json:"user_id"`
+	RefreshToken string    `json:"refresh_token"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+func FromEntity(session entity.Session) Session {
+	return Session{
+		ID:           session.ID.Value(),
+		UserID:       session.UserID.Value(),
+		RefreshToken: session.RefreshToken,
+		ExpiresAt:    session.ExpiresAt,
+		CreatedAt:    session.CreatedAt,
+	}
+}

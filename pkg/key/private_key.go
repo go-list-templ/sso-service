c50c6a3ed1753b428ac78f3f0fcc6d7ca52e1df3
Denis <korbakovd@gmail.com>
2026-05-08 00:43:35 +0300

    fix: update load private key

--- pkg/key/private_key.go
+++ pkg/key/private_key.go
@@ -2,6 +2,8 @@ package key
 
 import (
 	"crypto/rsa"
+	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/go-list-templ/sso-service/pkg/config"
@@ -21,16 +23,40 @@ func NewPrivate(cfg *config.PrivateKey) (*Private, error) {
 	return &Private{pk}, nil
 }
 
+type Keys struct {
+	CurrentVersion int        `json:"current_version"`
+	Versions       []*Version `json:"versions"`
+}
+
+type Version struct {
+	Version    int    `json:"version"`
+	PrivateKey string `json:"private_key"`
+	PublicKey  string `json:"public_key"`
+}
+
 func loadKey(cfg *config.PrivateKey) (*rsa.PrivateKey, error) {
-	data, err := os.ReadFile(cfg.File)
+	data, _ := os.ReadFile(cfg.File)
+
+	var keys Keys
+	err := json.Unmarshal(data, &keys)
 	if err != nil {
 		return nil, err
 	}
 
-	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(data)
-	if err != nil {
-		return nil, err
+	for _, version := range keys.Versions {
+		if nil == version {
+			continue
+		}
+
+		privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(version.PrivateKey))
+		if err != nil {
+			return nil, fmt.Errorf("parse private key version: %v  %w", version.Version, err)
+		}
+
+		if keys.CurrentVersion == version.Version {
+			return privateKey, nil
+		}
 	}
 
-	return privateKey, nil
+	return nil, fmt.Errorf("private key not found")
 }

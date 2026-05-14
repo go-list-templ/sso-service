d8486e3088ed2f1eaaa8c6ea6f3abff4cba71ed9
Denis <korbakovd@gmail.com>
2026-05-15 01:41:09 +0300

    fix: update logic for handling errors

--- internal/adapter/persistence/mongo/repo/session.go
+++ internal/adapter/persistence/mongo/repo/session.go
@@ -2,41 +2,43 @@ package repo
 
 import (
 	"context"
-	"github.com/go-list-templ/sso-service/internal/adapter/persistence/mongo"
+	"errors"
+
+	mongoClient "github.com/go-list-templ/sso-service/internal/adapter/persistence/mongo"
+
 	"github.com/go-list-templ/sso-service/internal/adapter/persistence/mongo/repo/dao"
 	"github.com/go-list-templ/sso-service/internal/core/domain/entity"
+	"github.com/go-list-templ/sso-service/internal/core/domain/entityerr"
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.uber.org/zap"
 )
 
 const Collection = "sessions"
 
 type Session struct {
-	*mongo.Mongo
+	*mongoClient.Mongo
 
 	logger *zap.Logger
 }
 
-func NewSession(m *mongo.Mongo, l *zap.Logger) *Session {
+func NewSession(m *mongoClient.Mongo, l *zap.Logger) *Session {
 	return &Session{m, l}
 }
 
 //TODO auto delete in mongo after refresh token expired
 
-func (s Session) Store(ctx context.Context, session entity.Session) error {
+func (s *Session) Store(ctx context.Context, session entity.Session) error {
 	collection := s.Database.Collection(Collection)
 
 	sessionDAO := dao.FromEntity(session)
 
 	_, err := collection.InsertOne(ctx, sessionDAO)
-	if err != nil {
-		s.logger.Error("insert session", zap.Any("ctx", ctx), zap.Error(err))
-	}
 
-	return err
+	return s.toMongoError(ctx, err)
 }
 
-func (s Session) FindAndDelete(ctx context.Context, refreshToken string) (entity.Session, error) {
+func (s *Session) FindAndDelete(ctx context.Context, refreshToken string) (entity.Session, error) {
 	collection := s.Database.Collection(Collection)
 
 	filter := bson.M{"refresh_token": refreshToken}
@@ -45,11 +47,22 @@ func (s Session) FindAndDelete(ctx context.Context, refreshToken string) (entity
 
 	err := collection.FindOneAndDelete(ctx, filter).Decode(&sessionDAO)
 	if err != nil {
-		//todo add error handling on not found
-		s.logger.Error("delete session", zap.Any("ctx", ctx), zap.Error(err))
-
-		return entity.Session{}, err
+		return entity.Session{}, s.toMongoError(ctx, err)
 	}
 
 	return sessionDAO.ToEntity(), nil
+}
+
+func (s *Session) toMongoError(ctx context.Context, err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		return entityerr.ErrSessionNotFound
+	}
+
+	s.logger.Error("mongo", zap.Any("context", ctx), zap.Error(err))
+
+	return err
 }

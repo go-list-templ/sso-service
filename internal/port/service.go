553271dbff01856b8560d9d03f190296babfc7d7
Denis <korbakovd@gmail.com>
2026-05-17 01:01:35 +0300

    feat: add new service

--- internal/port/service.go
+++ internal/port/service.go
@@ -2,13 +2,20 @@ package port
 
 import (
 	"context"
+
 	"github.com/go-list-templ/sso-service/internal/core/dto"
 )
 
 //go:generate mockgen -source=service.go -destination=mock/mock_service.go -package=mock
 
-type AuthService interface {
-	Register(context.Context, dto.AuthInput) (dto.AuthOutput, error)
-	Login(context.Context, dto.AuthInput) (dto.AuthOutput, error)
-	Refresh(context.Context, dto.RefreshInput) (dto.AuthOutput, error)
-}
+type (
+	AuthService interface {
+		Register(context.Context, dto.AuthInput) (dto.AuthOutput, error)
+		Login(context.Context, dto.AuthInput) (dto.AuthOutput, error)
+		Refresh(context.Context, dto.RefreshInput) (dto.AuthOutput, error)
+	}
+
+	JWKSService interface {
+		Get(context.Context) ([]dto.VaultPublicKey, error)
+	}
+)

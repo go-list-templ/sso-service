f8ab5a9bf6a4905ec9900465f4c0ce88574af8db
Denis <korbakovd@gmail.com>
2026-05-20 17:42:42 +0300

    feat: add logic for read old version and get log about rotate key

--- pkg/vault/transit_version.go
+++ pkg/vault/transit_version.go
@@ -1,41 +1,84 @@
 package vault
 
 import (
+	"context"
 	"errors"
 	"fmt"
-	"github.com/go-list-templ/sso-service/pkg/config"
 	"os"
 	"strconv"
 	"strings"
 	"sync/atomic"
+
+	"github.com/go-list-templ/sso-service/pkg/config"
+	"go.uber.org/zap"
 )
 
 type Transit struct {
-	cfg     *config.Vault
-	version atomic.Pointer[string]
+	cfg    *config.Vault
+	logger *zap.Logger
+
+	cachedVersion atomic.Pointer[string]
 }
 
-func NewTransit(cfg *config.Vault) *Transit {
-	return &Transit{cfg: cfg, version: atomic.Pointer[string]{}}
+func NewTransit(cfg *config.Vault, l *zap.Logger) *Transit {
+	return &Transit{cfg, l, atomic.Pointer[string]{}}
 }
 
-func (t *Transit) Version() (string, error) {
+func (t *Transit) Version(ctx context.Context) (string, error) {
 	data, err := os.ReadFile(t.cfg.TransitVersionPath)
 	if err != nil {
-		return "", fmt.Errorf("read transit version: %w", err)
+		cacheVersion := t.cacheVersion(ctx)
+		if cacheVersion == "" {
+			return "", fmt.Errorf("read transit version: %w", err)
+		}
+
+		return cacheVersion, nil
 	}
 
 	version := strings.TrimSpace(string(data))
 	if version == "" {
-		return "", errors.New("version is empty")
+		cacheVersion := t.cacheVersion(ctx)
+		if cacheVersion == "" {
+			return "", errors.New("version is empty")
+		}
+
+		return cacheVersion, nil
 	}
 
 	_, err = strconv.ParseInt(version, 10, 64)
 	if err != nil {
-		return "", fmt.Errorf("version is invalid: %w", err)
+		cacheVersion := t.cacheVersion(ctx)
+		if cacheVersion == "" {
+			return "", fmt.Errorf("version is invalid: %w", err)
+		}
+
+		return cacheVersion, nil
 	}
 
-	t.version.Store(&version)
+	oldVersionPtr := t.cachedVersion.Load()
+	if oldVersionPtr == nil || *oldVersionPtr != version {
+		t.logger.Info(
+			"rotate version key",
+			zap.Any("context", ctx),
+			zap.Any("old version", oldVersionPtr),
+			zap.String("new version", version),
+		)
+
+		t.cachedVersion.Store(&version)
+	}
 
 	return version, nil
+}
+
+func (t *Transit) cacheVersion(ctx context.Context) string {
+	version := t.cachedVersion.Load()
+	if version != nil && *version != "" {
+		t.logger.Error("get cache version", zap.Any("context", ctx), zap.String("version", *version))
+
+		return *version
+	}
+
+	t.logger.Error("empty cache version", zap.Any("context", ctx))
+
+	return ""
 }
